1d3e64386e3c6a2d442e0f7446309a77fc560016
Brandon <brandlerson@gmail.com>
2025-06-23 15:42:01 -0500

    chore: update config files for npm publishing

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { ParsedCWRFile } from './src/index.js';
-import { CWRParserNode } from './src/node/CWRParserNode.js';
-
-const parser = new CWRParserNode();
-
-(async () => {
-  const results: ParsedCWRFile = await parser.parseFile('./CW250645WD_000.V21');
-  console.log(results);
-})();
